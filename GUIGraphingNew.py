f6c8bfb37dfb1da3a9ffb8169b53163d686cea1f
Unknown <saketh.kollu@gmail.com>
2017-11-30 21:45:03 -0800

    Updated n intrerval simpson into Gui

--- GUIGraphingNew.py
+++ GUIGraphingNew.py
@@ -229,8 +229,8 @@ def animate(i):
         Y = Equation(eq, xmin, xmax, ymin, ymax)
 
         global integral
-        integral = Y.AccurateIntegration(A, B, 1)
-        inaccurateIntegral = Y.Integrate(A, B, 1)
+
+
 
         ax.plot(Y.getDomain(), Y.getYFunction(), "red")
 
@@ -242,6 +242,7 @@ def animate(i):
         plt.scatter(Y.getExtremaCoor()[0], Y.getExtremaCoor()[1], c="blue", s=25)
         plt.scatter(Y.getInflectionCoor()[0], Y.getInflectionCoor()[1], c="green", s=25)
 
+        integral = Y.nsimpson(A, B, 1000)
         print("Integral from A (", str(A) + " ) to B (", str(B) + " ) of f'(x) is " , round(integral,5))
         print("Showing FTC...")
         print("f(", str(B) + " ) -  f(", str(A) + " ) = ", str(round(Y.evaluate(B) - Y.evaluate(A),5 )))
